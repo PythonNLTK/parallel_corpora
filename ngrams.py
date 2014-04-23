55867aa4fce587742b0b73c8c0944891bb1c4a79
PythonNLTK <mike.zubi@gmail.com>
2014-04-23 23:28:44 +0200

    ngram MP

---
+++ ngrams.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+from __future__ import division
+
+import time, nltk
+from multiprocessing import Pool
+from nltk.corpus import brown
+
+t1 = time.time()
+
+data = brown.words(fileids=['ca01'])
+
+def find_ngrams(input_list, n):
+    return zip(*[input_list[i:] for i in range(n)])
+  
+ngrams = find_ngrams(data, 5)
+tagged = []
+for elem in ngrams:
+    tagged.append(nltk.pos_tag(elem))
+
+
+
+def findCandidates(ngram):
+    ngrams = []
+    ngrams.append(ngram)
+    
+    return ngrams
+
+if __name__ == '__main__':
+    pool = Pool(processes=1)
+    res = pool.map(findCandidates, tagged)
+    
+    print res
+    
+print time.time() - t1
