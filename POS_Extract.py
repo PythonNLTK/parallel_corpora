0a423de352361f11ba291c3597bb46b8c6f5d9a1
PythonNLTK <mike.zubi@gmail.com>
2014-04-23 21:57:32 +0200

    extended rules

--- POS_Extract.py
+++ POS_Extract.py
@@ -6,22 +6,15 @@ from nltk.corpus import brown
 from nltk import FreqDist
 import time, nltk
 # from collections import defaultdict
-from nltk.stem.wordnet import WordNetLemmatizer
 
 t1 = time.time()
 
-lmtzr = WordNetLemmatizer()
-
-data_tok = brown.words()
-
 #data_tok = nltk.word_tokenize(data)
-data_lower = []
-for token in data_tok:
-    data_lower.append(token.lower())
+data_tok = brown.words()
+#data_tok = brown.words(fileids=['ca01', 'ca02', 'cd01', 'cd02', 'ch01', 'ch02'])
+#data_lower = [token.lower() for token in data_tok]
     
-#data_lemma = [lmtzr.lemmatize(token) for token in data_lower]
-    
-pos_tagged = nltk.pos_tag(data_lower)
+pos_tagged = nltk.pos_tag(data_tok)
 
 term_freq = []
 
@@ -30,15 +23,19 @@ for i in range(0, len(pos_tagged) - 2):
 #         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0], pos_tagged[i+4][0]))
     if pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN' and pos_tagged[i+3][1] == 'NN':
         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
-    elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN':
-        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+#     elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN':
+#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
     elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN':
         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0]))
+#     elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'IN' and pos_tagged[i+2][1] == 'NN':
+#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+    elif (pos_tagged[i][1] == 'JJ' or pos_tagged[i][1] == 'NN') and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN':
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
                         
 fdist = FreqDist(term_freq)
 
-for i in range(0, 50):
-    print fdist[i]
+for i in range(0, 20):
+    print fdist.items()[i]
 
 print time.time() - t1
     
