052516e9706d05cfa6b20eb4841b4dfd802e4763
PythonNLTK <mike.zubi@gmail.com>
2014-04-25 11:35:18 +0200

    optmized code

--- TFIDF.py
+++ TFIDF.py
@@ -4,51 +4,78 @@ from __future__ import division
 from nltk.corpus import brown
 from nltk.corpus import stopwords
 from nltk import FreqDist
-import time, nltk, math, operator, codecs
+import time, nltk, math, operator, codecs, re
 from collections import defaultdict
 from nltk.stem.wordnet import WordNetLemmatizer
 
 lmtzr = WordNetLemmatizer()
 
-t1 = time.time()
-
-fdist = FreqDist()
-
-test_set = codecs.open('/home/michi/corpora/testset', 'r', 'utf8')
-data = test_set.read()
-
-data_tokenized = nltk.word_tokenize(data)
-data_lowercased = [token.lower() for token in data_tokenized]
-data_lemmas = [lmtzr.lemmatize(token) for token in data_lowercased]
-data_tagged = nltk.pos_tag(data_tokenized)
-# tagged_dict = {}
-# for entry in data_tagged:
-#     tagged_dict[lmtzr.lemmatize(entry[0])] = entry[1]
-
-for token in data_lemmas:
-    fdist.inc(token)
+def prepareData():
+        
+    test_set = codecs.open('/home/michi/corpora/testset', 'r', 'utf8')
+    data = test_set.read()
     
-doc_dic = defaultdict(list)
-for fileid in brown.fileids():
-    for token in brown.words(fileids=[fileid]):
-        if token in doc_dic:
-            if not fileid in doc_dic[token]:
-                doc_dic[lmtzr.lemmatize(token)].append(fileid)
-        else:
-            doc_dic[lmtzr.lemmatize(token)] = [fileid]
+    data_tokenized = nltk.word_tokenize(data)
+    data_lowercased = [token.lower() for token in data_tokenized]
+    data_lemmas = [lmtzr.lemmatize(token) for token in data_lowercased]
 
-tfidf = {}
-for token in data_lemmas:
-    max_value = max(fdist.values())
-    tfidf[token] = (fdist[token]/len(fdist)) * (math.log(500 / (1 + len(doc_dic[token]))))
+    #data_tagged = nltk.pos_tag(data_tokenized)
+    # tagged_dict = {}
+    # for entry in data_tagged:
+    #     tagged_dict[lmtzr.lemmatize(entry[0])] = entry[1]
+
+    return data_lemmas
+
+def calcFreq(data_lemmas):
+    fdist = FreqDist()
+    for token in data_lemmas:
+        fdist.inc(token)
+        
+    return fdist
+
+def calcDF():
     
-for k in tfidf.keys():
-    if k in stopwords.words('english') or k in [',', '.', ':', ';', '(', ')', '[', ']', "''", "``"]:
-        del tfidf[k]
+    doc_dic = defaultdict(set)
+    for fileid in brown.fileids():
+        for token in brown.words(fileids=[fileid]):
+            doc_dic[lmtzr.lemmatize(token)].add(fileid)
+            
+    return doc_dic
 
-sorted_tfidf = sorted(tfidf.iteritems(), key=operator.itemgetter(1))
 
-for entry in sorted_tfidf:
-    print entry
+def calcTFIDF(data_lemmas):
+    tfidf = {}
+    tokCount = len(fdist)
+    for token in set(data_lemmas):
+        tfidf[token] = (fdist[token]/tokCount) * (math.log(500 / (1 + len(doc_dic[token]))))
+        
+    return tfidf
+        
+def filterResults(tfidf):
+    nonPunct = re.compile('[A-Za-z]')
+    for k in tfidf.keys():
+        if k in stopwords.words('english') or not nonPunct.match(k):
+            del tfidf[k]
+            
+    return tfidf
 
-print time.time() - t1
+def output(tfidf_filtered):
+    sorted_tfidf = sorted(tfidf.iteritems(), key=operator.itemgetter(1))
+
+    for entry in sorted_tfidf:
+        print entry
+
+if __name__ == '__main__':
+    
+    t1 = time.time()
+    
+    data_lemmas = prepareData()
+    fdist = calcFreq(data_lemmas)
+    doc_dic = calcDF()
+    
+    tfidf = calcTFIDF(data_lemmas)
+    
+    tfidf_filtered = filterResults(tfidf)
+    output(tfidf_filtered)
+        
+    print time.time() - t1
