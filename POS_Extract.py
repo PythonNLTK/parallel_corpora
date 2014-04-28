73267e6f1dc5e38d2be104ec2d475e2eb2f522c7
PythonNLTK <mike.zubi@gmail.com>
2014-04-28 23:01:15 +0200

    updated rules

--- POS_Extract.py
+++ POS_Extract.py
@@ -20,24 +20,68 @@ pos_tagged = nltk.pos_tag(data_tok)
 
 term_freq = []
 
-for i in range(0, len(pos_tagged) - 2):
-#     if pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN' and pos_tagged[i+3][1] == 'NN' and pos_tagged[i+4][1] == 'NN':
+rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+for i in range(0, len(pos_tagged) - 5):
+    
+#     if (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS') and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS') and (pos_tagged[i+4][1] == 'NN' or pos_tagged[i+4][1] == 'NNS'):
 #         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0], pos_tagged[i+4][0]))
-    if pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN' and pos_tagged[i+3][1] == 'NN':
+#         rule1 += 1
+#         
+#     elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and pos_tagged[i+1][1] == 'IN' and pos_tagged[i+2][1] == 'DT' and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS') and (pos_tagged[i+4][1] == 'NN' and pos_tagged[i+4][1] == 'NNS'):
+#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0], pos_tagged[i+4][0]))
+#         rule2 += 1
+#         
+    if (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and pos_tagged[i+1][1] == 'IN' and pos_tagged[i+2][1] == 'DT' and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS'):
         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
-#     elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN':
-#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
-    elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'NN':
-        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0]))
-#     elif pos_tagged[i][1] == 'NN' and pos_tagged[i+1][1] == 'IN' and pos_tagged[i+2][1] == 'NN':
-#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
-    elif (pos_tagged[i][1] == 'JJ' or pos_tagged[i][1] == 'NN') and pos_tagged[i+1][1] == 'NN' and pos_tagged[i+2][1] == 'NN':
+        rule3 += 1
+         
+    elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and pos_tagged[i+2][1] == 'IN' and pos_tagged[i+3][1] == 'DT' and (pos_tagged[i+4][1] == 'NN' or pos_tagged[i+4][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0], pos_tagged[i+4][0]))
+        rule4 += 1
+      
+    elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and pos_tagged[i+1][1] == 'IN' and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS') and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
+        rule5 += 1
+        
+    elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and pos_tagged[i+1][1] == 'IN' and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS'):
         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+        rule6 += 1
+            
+#     elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS') and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS'):
+#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
+#         rule7 += 1
+#         
+#     elif pos_tagged[i][1] == 'JJ' and pos_tagged[i+1][1] == 'JJ' and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS') and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS'):
+#         term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
+#         rule8 += 1
+#         
+    elif pos_tagged[i][1] == 'JJ' and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and pos_tagged[i+2][1] == 'IN'  and (pos_tagged[i+3][1] == 'NN' or pos_tagged[i+3][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0], pos_tagged[i+3][0]))
+        rule9 += 1
+        
+    elif pos_tagged[i][1] == 'JJ' and pos_tagged[i+1][1] == 'JJ' and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+        rule10 += 1
+
+    elif pos_tagged[i][1] == 'JJ' and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+        rule11 += 1
+        
+    elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and (pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS') and (pos_tagged[i+2][1] == 'NN' or pos_tagged[i+2][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0], pos_tagged[i+2][0]))
+        rule13 += 1
+        
+    elif (pos_tagged[i][1] == 'NN' or pos_tagged[i][1] == 'NNS') and ( pos_tagged[i+1][1] == 'NN' or pos_tagged[i+1][1] == 'NNS'):
+        term_freq.append((pos_tagged[i][0], pos_tagged[i+1][0]))
+        rule12 += 1
                         
 fdist = FreqDist(term_freq)
 
-for i in range(0, 20):
+for i in range(0, 100):
     print fdist.items()[i]
+
+print rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13
 
 print time.time() - t1
     
