4c32f6791af98a80d2e54cd7961dafe42d29ff47
PythonNLTK <mike.zubi@gmail.com>
2014-05-02 16:18:53 +0200

    mergeGiza

---
+++ mergeGiza.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+from __future__ import division
+import codecs
+#from collections import defaultdict
+                 
+def readFile(inputFile):
+    
+    # data[0] = id, data[1] = string, data[2] = occurrence
+    handle = codecs.open(inputFile, 'r', 'utf8')
+    data = []
+    for line in handle.readlines():
+        data.append(line.split(' '))
+        
+    handle.close()
+    
+    return data
+
+def readAlignment(inputFile):
+    handle = codecs.open(inputFile, 'r', 'utf8')
+    data = {}
+    
+    for line in handle.readlines():
+        if not line.split(' ')[0] in data and 'e' in line.split(' ')[2]:
+            data[line.split(' ')[0]] = (line.split(' ')[1], 0)
+        if not line.split(' ')[0] in data and not 'e' in line.split(' ')[2]:
+            data[line.split(' ')[0]] = (line.split(' ')[1], line.split(' ')[2])
+        elif line.split(' ')[2] > data[line.split(' ')[0]][1] and not 'e' in line.split(' ')[2]:
+            data[line.split(' ')[0]] = (line.split(' ')[1], line.split(' ')[2])
+            
+    handle.close()
+            
+    return data
+
+if __name__ == '__main__':
+    
+    sourceFile = '/home/michi/working/source.txt'
+    targetFile = '/home/michi/working/target.txt'
+    alignmentFile = '/home/michi/working/aligned.txt'
+    
+    sourceLang = readFile(sourceFile)
+    targetLang = readFile(targetFile)
+    aligned = readAlignment(alignmentFile)
+        
+    for k, v in sorted(aligned.items()):
+        print k, v
+    
+    
