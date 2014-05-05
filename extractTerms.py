77f899ec6c0ff1cbbb093321d98e0c33a82b2b56
PythonNLTK <mike.zubi@gmail.com>
2014-05-05 21:56:46 +0200

    updated

--- extractTerms.py
+++ extractTerms.py
@@ -5,10 +5,11 @@ import codecs
 def readSingleWords():
     
     source = '/home/michi/corpora/tfidf_results.txt'
-    singleWordList = []
+    singleWordList = {}
     with codecs.open(source, 'r', encoding='utf-8') as myFile:
         for line in myFile:
-            singleWordList.append(line.split(','))
+            #singleWordList.append(line.split(','))
+            singleWordList[line.split(',')[0]] = line.split(',')[1]
         
     # [0] = string, [1] = score
     return singleWordList
@@ -24,6 +25,31 @@ def readMultiWords():
     # [0] = term, [1] = count
     return multiWordList
 
+def readTokenAlignment():
+    
+    source = '/home/michi/corpora/merged.txt'
+    alignedTokens = []
+    with codecs.open(source, 'r', encoding='utf-8') as myFile:
+        for line in myFile:
+            alignedTokens.append(line.split(','))
+
+    return alignedTokens
+
+def filterSingleTerms():
+    #to be done
+    pass
+    
+def filterMultiTerms():
+    #to be done
+    pass
+
 if __name__ == '__main__':
     
-    print readSingleWords()[-10:]
+    singleWordTerms = readSingleWords()
+    alignedData = readTokenAlignment()
+    
+#     for term in singleWordTerms:
+#         if term in alignedData:
+#             print term
+    
+    print singleWordTerms
