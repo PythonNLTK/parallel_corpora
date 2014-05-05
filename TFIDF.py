8897335010ea4c0703f0b3c9de1b57a9ca306fde
PythonNLTK <mike.zubi@gmail.com>
2014-05-05 21:38:27 +0200

    final

--- TFIDF.py
+++ TFIDF.py
@@ -59,15 +59,14 @@ def filterResults(tfidf):
             
     return tfidf
 
-def output(sorted):
-    for entry in sorted:
-        print entry
+# def output(sorted):
+#     for entry in sorted:
+#         print entry
         
 def writeToFile(sorted_output):
-    with codecs.open('/home/michi/corpora/tfidf.txt', 'a', encoding='utf-8') as outputFile:
+    with codecs.open('/home/michi/corpora/tfidf.txt', 'w', encoding='utf-8') as outputFile:
         for entry in sorted_output:
             outputFile.write(entry[0] + ',' + str(entry[1]) + '\n')
-            #outputFile.write('\n')    
 
 
 if __name__ == '__main__':
