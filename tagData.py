3c4d150b9da2f37a7e4413c2c70d527ed2b602ac
PythonNLTK <mike.zubi@gmail.com>
2014-05-11 19:27:54 +0200

    tokenize and POS tag data

---
+++ tagData.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+from __future__ import division
+import nltk, codecs
+
+def tokenize(rawData):
+    
+    return nltk.word_tokenize(rawData)
+    
+def tag(tokenizedData):
+    
+    return nltk.pos_tag(tokenizedData)
+    
+def writeToFile(taggedData):
+    
+    outputFile = '/home/michi/corpora/testset_tagged'
+    with codecs.open(outputFile, 'w', encoding='utf-8') as myFile:
+        for tup in taggedData:
+            myFile.write(tup[0] + ',' + tup[1] + '\n')
+    
+if __name__ == '__main__':
+    
+    inputFile = '/home/michi/corpora/testset'
+
+    handle = codecs.open(inputFile, 'r', encoding='utf-8')
+    raw = handle.read()
+    handle.close()
+    tokenized = tokenize(raw)
+    tagged = tag(tokenized)
+    writeToFile(tagged)
+    
+    
+    
